e4c03ac20f6c610c9636aeb734e957291af92e3f
Michael Mecham <myke.mecham@gmail.com>
2016-02-01 21:01:48 -0700

    Added Flashlight funcitonality
    Built the Flashlight app.

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var isOn = false
+    
+    
+    
+    @IBOutlet weak var buttonTapped: UIButton!
+    @IBAction func buttonTapped(sender: AnyObject) {
+        
+        if isOn {
+        isOn = false
+        self.view.backgroundColor = UIColor.blackColor()
+        buttonTapped.setTitle("On", forState: .Normal)
+        buttonTapped.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        
+            
+        } else {
+            isOn = true
+            self.view.backgroundColor = UIColor.whiteColor()
+            buttonTapped.setTitle("Off", forState: .Normal)
+            buttonTapped.setTitleColor(UIColor.blackColor(), forState: .Normal)
+
+        }
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
